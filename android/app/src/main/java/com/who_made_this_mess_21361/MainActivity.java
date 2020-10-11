76b1f7b2d8f5d6263fb2be5cfb9ccd25fd818ce3
Crowdbotics <team@crowdbotics.com>
2020-10-11 16:00:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/who_made_this_mess_21361/MainActivity.java
@@ -0,0 +1,15 @@
+package com.who_made_this_mess_21361;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "who_made_this_mess_21361";
+    }
+}
---
+++ ios/who_made_this_mess_21361Tests/who_made_this_mess_21361Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface who_made_this_mess_21361 : XCTestCase
+
+@end
+
+@implementation who_made_this_mess_21361
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
